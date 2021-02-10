7cd14f54df19b17662a36847a0e44f0af99f9912
davide <davidecorsinimilano@gmail.com>
2021-02-10 19:08:36 +0100

    push modify

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ var app = new Vue({
         query: '',
         apiKey: 'dd971d8403d27ab0d69820d891653085',
         movies: [],
+        seriesTv: [],
         imgNull: 'pellicola.jpg',
-
         starValue: '',
         maxStars: 5,
-        arrayStelle: []
+        film: 'movie',
     },
     methods: {
         globalSearch(){//global search é una funzione global per la ricerca ad un evento keyup
             //Questa call é riferita solo e unicamente per i film
             axios
-                .get("https://api.themoviedb.org/3/search/movie", {
+                .get("https://api.themoviedb.org/3/search/" + this.film , {
                     params:{
                         api_key: this.apiKey,
                         query: this.query,
@@ -25,12 +25,20 @@ var app = new Vue({
             .then((result) => {
                 this.movies = result.data.results;
                 console.log(this.movies, 'NEI FILM');
+
+                this.movies = this.movies.map(element => {
+                    return {
+                        ...element,
+                        votoStelle: Math.round(element.vote_average / 2)
+                    }
+                })
+
             })
 
 
             //Call for tv shows
             axios
-                .get("https://api.themoviedb.org/3/search/tv",{
+                .get("https://api.themoviedb.org/3/search/tv", {
                     params:{
                         api_key: this.apiKey,
                         query: this.query,
@@ -44,26 +52,14 @@ var app = new Vue({
 
 
 
-                this.movies = this.movies.map(element => {
-                    return{
-                        ...element,
-                        votoStelle: Math.round(element.vote_average / 2)
-                    }
-                })
-
+        
                 
                 
             })
 
 
             
-        },
-        //funzione voto stars
-        //La posso mettere in un computed o in un mounted??? Da chiedere
-        // voteRating() {
-        //     return (Math.round(this.movies.vote_average) * this.maxStars) / 10;
-        //     console.log(this.movies.vote_average);
-        // }
+        }
     },
 
 
